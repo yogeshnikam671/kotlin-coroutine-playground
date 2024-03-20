c459be32b9fc8e1dbd73317d3158298c33c14945
Yogesh Nikam <yogeshnikam671@gmail.com>
2024-03-20 23:12:35 +0530

    Hello Coroutine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,16 @@
 package com.coroutines
 
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 fun main() {
+    GlobalScope.launch {
+        delay(1000)
+        println("Hello Coroutine!")
+    }
+
     println("Hello World!")
+
+    Thread.sleep(2000)
 }
